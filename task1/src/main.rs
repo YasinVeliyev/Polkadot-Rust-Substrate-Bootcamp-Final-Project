e5f227d0a26ff9292bbbeb98c1f8b34bd4bcef0b
YasinVeliyev <veliyev.yasin@gmail.com>
2023-08-18 21:17:12 +0400

    new file:   task1/README.md 	modified:   task1/src/main.rs

--- task1/src/main.rs
+++ task1/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
 
 
 fn concatenate_strings(str1:&str,str2:&str) ->String {
-    let mut result = String::from(str1);
+    let mut result = String::new();
+    result.push_str(str1);
     result.push_str(str2);
     result
 }
