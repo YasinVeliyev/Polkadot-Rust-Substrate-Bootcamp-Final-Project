b66fbc046f44b28cd143cfe74713ec0646aa2645
YasinVeliyev <veliyev.yasin@gmail.com>
2023-08-27 17:09:04 +0400

    modified:   task3/src/main.rs

--- task3/src/main.rs
+++ task3/src/main.rs
@@ -31,18 +31,17 @@ impl FilterCondition {
     fn is_match(&self, other: i32) -> bool {
         other % self.filter == 0
     }
-
-    fn custom_filter(&self, v: Vec<i32>) -> Vec<i32> {
-        v.iter()
-            .filter(|a| self.is_match(**a))
-            .map(|a| *a)
-            .collect()
-    }
 }
 
+fn custom_filter(v: Vec<i32>, filter: &FilterCondition) -> Vec<i32> {
+    v.iter()
+        .filter(|a| filter.is_match(**a))
+        .map(|a| *a)
+        .collect()
+}
 fn main() {
     let vector = vec![1, 2, 3, 4, 5, 6, 7, 8, 3, 8, 3, 9];
     let filter = FilterCondition::new(3);
-    let new_vec = filter.custom_filter(vector);
+    let new_vec = custom_filter(vector, &filter);
     println!("{:?}", new_vec);
 }
